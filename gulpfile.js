b792c6efe13c0ba71254fecd8a0b8ea71687b473
zhangrxiang <599490911@qq.com>
2017-12-28 21:04:28 +0800

    buffer

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,22 @@ var gulp = require('gulp');
 var exec = require('child_process');
 gulp.task('default', function (cb) {
     // 编译 Jekyll
-    exec.exec('ls ', function (err,stdout) {
-        if (err) return cb(err); // 返回 error
+    exec.exec('ls ', function (err, stdout) {
+        if (err)
+            return cb(err); // 返回 error
         console.log(stdout);
         cb(); // 完成 task
     });
+});
+
+gulp.task('assert', function (cb) {
+    exec.exec('node assert/index.js', function (err, stdout) {
+        if (err)
+            return cb(err);
+        else {
+            console.log(stdout);
+            cb();
+        }
+    })
 });
 
