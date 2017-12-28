5de02b08e7bca4d832f0c439b5ec362e9e0f49d7
zhangrxiang <599490911@qq.com>
2017-12-28 18:01:03 +0800

    init

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+// 在 shell 中执行一个命令
+
+var gulp = require('gulp');
+var exec = require('child_process');
+gulp.task('default', function (cb) {
+    // 编译 Jekyll
+    exec.exec('ls ', function (err,stdout) {
+        if (err) return cb(err); // 返回 error
+        console.log(stdout);
+        cb(); // 完成 task
+    });
+});
+
